a7836a69daa48823dbb76cae3e4f1b23be6f31e7
Luis Cerda <lgcerda@gmail.com>
2022-11-08 20:28:13 -0300

    Add mundial Function

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -63,3 +63,20 @@ response.setMensaje("ok");
 
 		return response;
 } 
+
+@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
+public @ResponseBody Mundial getTotalMundial (){
+
+		LOGGER.log(Level.INFO, "Consulta Mundial");
+
+		RestTemplate restTemplate = RestTemplate();
+	ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/world/total" ,String.class);
+	Mundial response = new Mundial();
+		Gson gson = new Gson();
+	Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
+	response.setTotalConfirmed(estado.getTotalConfirmed());
+	response.setTotalDeaths(estado.getTotalDeaths());
+	response.setTotalRecovered(estado.getTotalRecovered());
+
+			return response;
+}
